3ed49d3b321f3290d7d61f8eb21d9576b1e74dba
yoann <yoann@simprints.com>
2023-07-06 11:50:53 +0200

    Upgrade dependencies and go version. Remove ginkgo and gomega to use testify for testing. Replace gcp project with an existing one

--- internal/mywebapp/users/users_test.go
+++ internal/mywebapp/users/users_test.go
@@ -5,92 +5,48 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
-	"mywebapp/internal/mywebapp"
-	"mywebapp/internal/mywebapp/users"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"mywebapp/internal/mywebapp"
+	"mywebapp/internal/mywebapp/users"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMyWebApp(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "test suite for mywebapp")
+	t.Parallel()
+
+	user := users.User{
+		Id:    string(rand.Int31()),
+		Email: "thisisanemail@domain.com",
+	}
+	marshaledUser, err := json.Marshal(user)
+	require.NoError(t, err)
+
+	server := httptest.NewServer(mywebapp.NewController())
+
+	// Creating a user
+	resp, err := http.Post(fmt.Sprintf("%s/users", server.URL), "application/json", bytes.NewReader(marshaledUser))
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	// Getting a user
+	resp, err = http.Get(fmt.Sprintf("%s/users/%s", server.URL, user.Id))
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	// Checking body
+	var unmarshaledUser users.User
+	require.NoError(t, json.NewDecoder(resp.Body).Decode(&unmarshaledUser))
+	assert.Equal(t, user, unmarshaledUser)
+
+	// Deleting a user
+	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/users/%s", server.URL, user.Id), nil)
+	require.NoError(t, err)
+	resp, err = http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
 }
-
-var _ = Describe("my web app", func() {
-	var server *httptest.Server
-	BeforeEach(func() {
-		server = httptest.NewServer(mywebapp.NewController())
-	})
-	AfterEach(func() {
-		server.Close()
-	})
-
-	Describe("the standard user lifecycle", func() {
-		var user = users.User{
-			Id:    string(rand.Int31()),
-			Email: "thisisanemail@domain.com",
-		}
-		userJson, _ := json.Marshal(user)
-
-		Describe("creating a user", func() {
-			var (
-				resp *http.Response
-				err  error
-			)
-
-			BeforeEach(func() {
-				resp, err = http.Post(fmt.Sprintf("%s/users", server.URL), "application/json", bytes.NewReader(userJson))
-			})
-
-			It("should return a 201", func() {
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.StatusCode).To(BeIdenticalTo(http.StatusCreated))
-			})
-		})
-
-		Describe("getting the user", func() {
-			var (
-				resp *http.Response
-				err  error
-			)
-
-			BeforeEach(func() {
-				resp, err = http.Get(fmt.Sprintf("%s/users/%s", server.URL, user.Id))
-			})
-
-			It("should return a 200", func() {
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.StatusCode).To(BeIdenticalTo(http.StatusOK))
-			})
-
-			It("should return the JSON encoded user as its body", func() {
-				decodedUser := users.User{}
-				err := json.NewDecoder(resp.Body).Decode(&decodedUser)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(decodedUser).To(BeIdenticalTo(user))
-			})
-		})
-
-		Describe("deleting the user", func() {
-			var (
-				resp *http.Response
-				err  error
-			)
-
-			BeforeEach(func() {
-				req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/users/%s", server.URL, user.Id), nil)
-				Expect(err).ToNot(HaveOccurred())
-				resp, err = http.DefaultClient.Do(req)
-			})
-
-			It("should return a 204", func() {
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.StatusCode).To(BeIdenticalTo(http.StatusNoContent))
-			})
-		})
-	})
-})
